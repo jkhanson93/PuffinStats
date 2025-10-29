dcbfec68528bf02e8cf5c6d4037aa39f4047a9f2
Joel Hanson <jkhanson93@users.noreply.github.com>
2025-10-29 13:22:49 -0700

    updated UI, added puffin intake and output options, added status display

--- main.py
+++ main.py
@@ -8,24 +8,56 @@ app = Flask(__name__)
 def home():
     return "Hello, Puff!"
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
+@app.route('/api/lastFed', methods=['GET'])
+def get_last_fed():
     # Example: Fetch data from a database or perform a calculation
     fed_time = ""
     with open("fedpuff.txt", "r") as file:
         fed_time = file.read()
-    data = {'message': 'Puffin was last fed at:', 'value': fed_time}
+    data = {'value': f"Puffin was last fed at: {fed_time}"}
     return jsonify(data)
 
-@app.route('/api/process', methods=['POST'])
-def process_data():
-    input_data = request.json
+@app.route('/api/feed', methods=['POST'])
+def feed_puff():
     curr_datetime = datetime.now()
     formatted_dt = curr_datetime.strftime("%H:%M on %m-%d-%Y")
     with open("fedpuff.txt", "w") as file:
         file.write(formatted_dt)
     # Example: Process the input data
-    result = {'processed_message': f"You fed the Puff at {formatted_dt}"}
+    result = {'message': f"You fed the Puff at {formatted_dt}"}
+    return jsonify(result)
+
+@app.route('/api/boxStatus', methods=['GET'])
+def get_box_status():
+    # Example: Fetch data from a database or perform a calculation
+    fed_time = ""
+    with open("puffbox.txt", "r") as file:
+        fed_time = file.read()
+    data = {'message': 'Puffin\'s box status:', 'value': fed_time}
+    return jsonify(data)
+
+@app.route('/api/cleanBox', methods=['POST'])
+def clean_box():
+    input_data = request.json
+    cleaner = input_data['person']
+    curr_datetime = datetime.now()
+    formatted_dt = curr_datetime.strftime("%H:%M on %m-%d-%Y")
+    with open("puffbox.txt", "w") as file:
+        file.write(f"{cleaner} cleaned Puff's box at {formatted_dt}")
+    # Example: Process the input data
+    result = {'message': f"{cleaner} cleaned Puff's box at {formatted_dt}"}
+    return jsonify(result)
+
+@app.route('/api/changeBox', methods=['POST'])
+def change_box():
+    input_data = request.json
+    cleaner = input_data['person']
+    curr_datetime = datetime.now()
+    formatted_dt = curr_datetime.strftime("%H:%M on %m-%d-%Y")
+    with open("puffbox.txt", "w") as file:
+        file.write(f"{cleaner} changed Puff's box at {formatted_dt}")
+    # Example: Process the input data
+    result = {'message': f"{cleaner} changed Puff's box at {formatted_dt}"}
     return jsonify(result)
 
 if __name__ == '__main__':
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,33 +4,94 @@ import requests
 from datetime import datetime
 
 st.title("Don't Let Puffin Lie To You")
+status = ""
 
-# Fetch data from Flask API
-if st.button("When did the Puff last eat?"):
-    response = requests.get("http://localhost:5000/api/data")
-    if response.status_code == 200:
-        data = response.json()
-        st.write(data['message'])
-        st.write(data['value'])
-    else:
-        st.error("No record of Puff's last meal")
-        st.error(f"Tech reason (for Joel): {response.status_code}")
-        with open("error_log.txt", "w") as file:
-            curr_dt = datetime.now()
-            formatted_dt = curr_dt.strftime("%H:%M on %m-%d-%Y")
-            file.write(f"Failed to find when Puff ate at {formatted_dt} with code {response.status_code}")
+left_col, right_col = st.columns(2)
 
-# # Send data to Flask API for processing
-# user_input = st.text_input("Enter a message to process:")
-if st.button("I just fed the Puff"):
-    payload = {'message': 'I fed Puff!'}
-    response = requests.post("http://localhost:5000/api/process", json=payload)
-    if response.status_code == 200:
-        result = response.json()
-        st.write(result['processed_message'])
-    else:
-        with open("error_log.txt", "w") as file:
-            curr_dt = datetime.now()
-            formatted_dt = curr_dt.strftime("%H:%M on %m-%d-%Y")
-            file.write(f"Failed to feed Puff at {formatted_dt} with code {response.status_code}")
-        st.error(f"Error processing data: {response.status_code}")
+with left_col:
+    st.header("Puffin Intake:")
+    # Fetch data from Flask API
+    if st.button("When did the Puff last eat?"):
+        response = requests.get("http://localhost:5000/api/lastFed")
+        if response.status_code == 200:
+            data = response.json()
+            status = data['value']
+        else:
+            st.error("No record of Puff's last meal")
+            st.error(f"Tech reason (for Joel): {response.status_code}")
+            with open("error_log.txt", "w") as file:
+                curr_dt = datetime.now()
+                formatted_dt = curr_dt.strftime("%H:%M on %m-%d-%Y")
+                file.write(f"Failed to find when Puff ate at {formatted_dt} with code {response.status_code}")
+
+    # # Send data to Flask API for processing
+    # user_input = st.text_input("Enter a message to process:")
+    if st.button("I just fed the Puff"):
+        payload = {'message': 'I fed Puff!'}
+        response = requests.post("http://localhost:5000/api/feed", json=payload)
+        if response.status_code == 200:
+            result = response.json()
+            status = result['message']
+        else:
+            with open("error_log.txt", "w") as file:
+                curr_dt = datetime.now()
+                formatted_dt = curr_dt.strftime("%H:%M on %m-%d-%Y")
+                file.write(f"Failed to feed Puff at {formatted_dt} with code {response.status_code}")
+            st.error(f"Error processing data: {response.status_code}")
+
+with right_col:
+    st.header("Puffin Output:")
+    # Fetch data from Flask API
+    if st.button("How's Puff's Box Doing?"):
+        response = requests.get("http://localhost:5000/api/boxStatus")
+        if response.status_code == 200:
+            data = response.json()
+            status = data['value']
+        else:
+            st.error("No record of Puff's box")
+            st.error(f"Tech reason (for Joel): {response.status_code}")
+            with open("error_log.txt", "w") as file:
+                curr_dt = datetime.now()
+                formatted_dt = curr_dt.strftime("%H:%M on %m-%d-%Y")
+                file.write(f"Failed to get Puff\'s box status at {formatted_dt} with code {response.status_code}")
+
+    user_input = st.selectbox(
+        'Who cleaned/changed Puff\'s Box?',
+        ['Joel', 'Lexi', 'A Stranger']
+    )
+    if st.button("Cleaned Puff's Box"):
+        if user_input:
+            payload = {'person': user_input}
+            response = requests.post("http://localhost:5000/api/cleanBox", json=payload)
+            if response.status_code == 200:
+                result = response.json()
+                status = result['message']
+            else:
+                with open("error_log.txt", "w") as file:
+                    curr_dt = datetime.now()
+                    formatted_dt = curr_dt.strftime("%H:%M on %m-%d-%Y")
+                    file.write(f"Failed to clean Puff\'s box at {formatted_dt} with code {response.status_code}")
+                st.error(f"Error processing data: {response.status_code}")
+        else:
+            st.error("Please enter a person")
+        
+
+    if st.button("Changed Puff's Box"):
+        if user_input:
+            payload = {'person': user_input}
+            response = requests.post("http://localhost:5000/api/changeBox", json=payload)
+            if response.status_code == 200:
+                result = response.json()
+                status = result['message']
+            else:
+                with open("error_log.txt", "w") as file:
+                    curr_dt = datetime.now()
+                    formatted_dt = curr_dt.strftime("%H:%M on %m-%d-%Y")
+                    file.write(f"Failed to change Puff\'s box at {formatted_dt} with code {response.status_code}")
+                st.error(f"Error processing data: {response.status_code}")
+        else:
+            st.error("Please enter a person")
+
+st.header("Puff Status:")
+if status:
+    st.write(status)
